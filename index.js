7218b2e87b5f4ee6428fd10802f2bf61aca77cb2
Michael Okoe <michael.okoejr@gmail.com>
2022-06-15 08:47:22 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+  function superbowlWin(array){
+      let matchedItem = array.find(record => {
+          return record.result === "W"
+      })
+      if(matchedItem){
+          return matchedItem.year
+      } else{
+          return matchedItem
+      }
+  }
